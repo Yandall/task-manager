ae62d47fbe487e43282d60dcd762a7b1d51e1c3a
Yandall <jhonstevenkill22@gmail.com>
2022-08-17 16:30:10 -0500

    Actualizacion de errores de postgres

--- src/common/exceptions/pg-exception.filter.ts
+++ src/common/exceptions/pg-exception.filter.ts
@@ -13,23 +13,17 @@ export class PgExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     if (exception instanceof QueryFailedError) {
-      const responseMessage: { message: string; statusCode: number } =
-        this.errorCodes[exception.driverError.code] || this.defaultError;
-      return response.status(responseMessage.statusCode).json(responseMessage);
+      const driverError = exception.driverError;
+      const responseMessage = {
+        code: driverError.code,
+        message: driverError.message,
+        error: {
+          constraint: driverError.constraint,
+          detail: driverError.detail,
+        },
+      };
+      return response.status(HttpStatus.BAD_REQUEST).json(responseMessage);
     }
     return response.status(exception.status).json(exception.response);
   }
-
-  private readonly defaultError = {
-    message: "Internal server error",
-    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-  };
-
-  private readonly errorCodes = {
-    23505: {
-      message: "Error duplicate value",
-      statusCode: HttpStatus.BAD_REQUEST,
-      error: "Bad Request",
-    },
-  };
 }
