b5b38008a73f54454650b65f644afc8a84264bd4
Yandall <jhonstevenkill22@gmail.com>
2022-09-02 14:50:30 -0500

    Metodo para checkear por la existencia de algun recurso en alguna entidad

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,5 +1,7 @@
 import { randomInt } from "crypto";
 import * as bcrypt from "bcrypt";
+import { ForbiddenException, NotFoundException } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 
 /**
  * Creates a random string
@@ -20,4 +22,28 @@ export function randomId(idSize = 12): string {
 export function hashPassword(password: string) {
   const salt = bcrypt.genSaltSync();
   return bcrypt.hashSync(password, salt);
+}
+
+/**
+ * Check if a given resource exists or is not deleted.
+ * Can throw an error if the resource is not valid or return boolean
+ * @param id Id of the resource to search
+ * @param entity Name of the entity where to check
+ * @param throwError If set to true throws error on not found or resource deleted. Default false
+ * @returns boolean indicating that the resource is valid or not.
+ */
+export async function checkForExistance(
+  id: string,
+  entity: string,
+  throwError = false
+) {
+  const prisma = new PrismaClient();
+  const search = await prisma[entity].findUnique({ where: { id } });
+  if (!search && throwError) throw new NotFoundException();
+  if (search.isDeleted && throwError)
+    throw new ForbiddenException(
+      "The resource is already deleted and cannot be edited",
+      `Resource (${id}) doesn't exists`
+    );
+  return search && !search.isDeleted;
 }
