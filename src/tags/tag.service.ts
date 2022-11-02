45f3374c5584f970b1bd9bbe1130bfcd7dcacb43
Yandall <jhonstevenkill22@gmail.com>
2022-11-01 23:25:25 -0500

    Error con nombre de propiedad de tags
    Error al traer tags ya eliminados

--- src/tags/tag.service.ts
+++ src/tags/tag.service.ts
@@ -10,14 +10,14 @@ export class TagsService {
   constructor(private prisma: PrismaService) {}
 
   getTagsByOwner(owner: number) {
-    return this.prisma.tags.findMany({ where: { owner } });
+    return this.prisma.tags.findMany({ where: { owner, isDeleted: false } });
   }
 
   create(createTagDto: CreateTagDto) {
     const data: Prisma.tagsCreateInput = {
       id: createTagDto.id,
       name: createTagDto.name,
-      config: createTagDto.config,
+      color: createTagDto.color,
       createdDate: createTagDto.createdDate,
       isDeleted: createTagDto.isDeleted,
       user: { connect: { id: createTagDto.owner } },
@@ -29,7 +29,7 @@ export class TagsService {
     await checkForExistance(updateTagDto.id, this.entityName, true);
     const data: Prisma.tagsUpdateInput = {
       name: updateTagDto.name,
-      config: updateTagDto.config,
+      color: updateTagDto.color,
       updatedDate: updateTagDto.updatedDate,
     };
     return this.prisma.tags.update({ where: { id: updateTagDto.id }, data });
